c086a6bc299ba092416431cf3ee9d800cc479600
Farman <farman.khan@inmobi.com>
2020-08-27 22:02:15 +0530

    enabled viewBinding and dataBinding

--- app/src/main/java/com/fkhan/shoppingmvvm/MainActivity.kt
+++ app/src/main/java/com/fkhan/shoppingmvvm/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.fkhan.shoppingmvvm
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fkhan.shoppingmvvm.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.txt.text = "this is shopping cart demo, yay!"
     }
 }
